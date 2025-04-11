322b54bc57e739dfd4eea595095d4c0f5b53dc31
MuriloDaCruz <murigatosouza@gmail.com>
2025-04-11 14:25:02 -0300

    Criação da model Categoria

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    Descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"({self.id}) {self.Descricao}"
+        return f"({self.id}) {self.descricao}"
