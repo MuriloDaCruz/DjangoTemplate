74d4f11e309030871590d1cb413735a5405dbf78
MuriloDaCruz <murigatosouza@gmail.com>
2025-05-20 09:33:38 -0300

    feat: Início da associação de imagens - contem ERRO no core

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -13,27 +16,31 @@ from core.views import CategoriaViewSet
 from core.views import EditoraViewSet
 from core.views import LivroViewSet
 
+from uploader.router import router as uploader_router
+
 router = DefaultRouter()
-router.register(r'autores', AutorViewSet)
-router.register(r'categorias', CategoriaViewSet)
-router.register(r'editoras', EditoraViewSet)
-router.register(r'livros', LivroViewSet)
-router.register(r'usuarios', UserViewSet, basename='usuarios')
+router.register(r"autores", AutorViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livros", LivroViewSet)
+router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # OpenAPI 3
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/swagger/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'api/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
+    path("api/media/", include(uploader_router.urls)),
     # API
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
